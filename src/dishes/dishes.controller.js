7d807b2ff8b5ddd62fa002410be8bac02eac08ee
Jenni Grubbs <jennigrubbsjrn@gmail.com>
2023-03-12 01:22:30 -0700

    Progress updates to dishes router, controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,86 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+// List all the dishes
+function list(req, res) {
+    res.json({ data: dishes })
+}
+
+// Checks for valid dish with specified properties inputed from the req body
+function dishHasRequiredProperties(req, res, next) {
+    const { data: { id, name, description, price, image_url } = { } } = req.body;
+    if ({ id, name, description, price, image_url }) {
+        return next()
+    }
+    next({
+        status: 400, 
+        message: "Dish must include `${{ id, name, description, price, image_url }}` properties.",
+    })
+}
+
+// Gets max ID from urls data
+let lastDishId = dishes.reduce((maxId, url) => Math.max(maxId, url.id), 0)
+
+// Creates new dish {} w/ID, then push into dishes data
+function create(req, res) {
+    const { data: { id, name, description, price, image_url } = { } } = req.body
+    const newDish = {
+        id: lastDishId++,
+        name, 
+        description, 
+        price, 
+        image_url,
+    }
+    dishes.push(newDish)
+    res.status(201).json({ data: newDish })
+}
+
+function read(req, res) {
+    const { dishId } = req.params
+    const dishNeeded = foundDish(dishId)
+    res.json({ data: dishNeeded })
+  }
+
+function sendDishes(req, res) {
+    const { dishToSend } = req.params
+    const allDishes = dishes.filter(dish => dish.dishId === Number(dishId))
+    res.json( { data: allDishes })
+}
+
+function readDishForDishId(req, res) {
+    const { dishId } = req.params
+    const resultDish = dishes.filter(dish => dish.dishId === Number(dishId)).find(dish => dish.id === Number(dishId))
+    res.json( { data: resultDish })
+}
+
+function update(req, res) {
+    const { dishId } = req.params
+    const foundDish = findDish(dishId)
+    const dishIndex = dishes.findIndex((dish) => dish.id === Number(dishId))
+    const { data: { id, name, description, price, image_url } = { } } = req.body
+    // Replace the old object at dishIndex
+    dishes.splice(dishIndex, 1, {
+      data: {
+          ...dish
+        }
+    });
+    res.json({ data: dish })
+}
+
+
+module.exports = {
+    create: [dishHasRequiredProperties, create],
+    list,
+    readDish: [dishExists, sendDishes],
+    readDishForDishId: [dishExists, readDishForDishId],
+    read: [dishExists, read],
+    update: [dishExists, dishHasRequiredProperties, update],
+    dishExists,
+}
+
+/*
+1.	In the src/dishes/dishes.controller.js file, add handlers 
+and middleware functions to create, read, update, and list dishes. 
+Note that dishes cannot be deleted.
+*/
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -2,4 +2,32 @@ const router = require("express").Router();
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 
+const controller = require("../dishes/dishes.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
+const errorHandler = require("../errors/errorHandler")
+const notFound = require("../errors/notFound")
+const ordersRouter = require("../orders/orders.router")
+
+router.dish("/:dishId/dishes", controller.dishExists, dishesRouter)
+
+router
+    .route("/dishes")
+    .post(controller.create)
+    .get(controller.list)
+    .all(methodNotAllowed)
+
+router
+    .route("/dishes/:dishId")
+    .get(controller.read)
+    .put(controller.update)
+    .all(methodNotAllowed)
+
 module.exports = router;
+
+
+/*
+2.	In the src/dishes/dishes.router.js file, add two routes: 
+/dishes, and /dishes/:dishId and attach the handlers 
+(create, read, update, and list) exported from 
+src/dishes/dishes.controller.js.
+*/
